064adb9d9cdc6040e11fd0f4c5d02089cff95775
liuxuewen_glb <liuxuewen_glb@kgc.com>
2020-12-15 17:07:32 +0800

    kmall-user-web 测试

--- kmall-user-web/src/main/java/com/kgc/kmall/user/controller/MemberController.java
+++ kmall-user-web/src/main/java/com/kgc/kmall/user/controller/MemberController.java
@@ -2,10 +2,11 @@ package com.kgc.kmall.user.controller;
 
 import com.kgc.kmall.bean.Member;
 import com.kgc.kmall.service.MemberService;
-import jdk.nashorn.internal.ir.annotations.Reference;
+import org.apache.dubbo.config.annotation.Reference;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.nio.channels.MembershipKey;
 import java.util.List;
@@ -14,7 +15,7 @@ import java.util.List;
  * @author shkstart
  * @create 2020-12-15 16:35
  */
-@Controller
+@RestController
 public class MemberController {
 
 
@@ -22,7 +23,6 @@ public class MemberController {
     MemberService memberService;
 
     @RequestMapping("/test")
-    @ResponseBody
     public String test(){
         List<Member> members = memberService.selectAllMember();
         if (members.size()>0){
